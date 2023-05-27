97799bb65e81529d42823d7e5a139001b96a99d2
programming-programmer <mthich15@gmail.com>
2023-05-26 21:00:09 -0700

    Add beautiful prompt via starship

--- nvim/lua/mthich15/remap.lua
+++ nvim/lua/mthich15/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("i", "<C-c>", "<Esc>")
-vim.keymap.set("n", "<C-c>", "i")
+vim.keymap.set("i", "<C-g>", "<Esc>")
